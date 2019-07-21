93c0b12d1c54fd9709201d9d6054af438d1e812d
Anonimo\Pablo <pablo.vazquez@alumnos.fesac.es>
2019-07-21 09:17:39 +0200

    Rocket sound

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    private AudioSource audioSource;
+
+    public float pushForce = 1;
+    public float rotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * pushForce);
+            if (!audioSource.isPlaying)
+            {
+            audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+         
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime) ;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
